a4d3b84680d143da63e09cdaa46d861ec6f77f13
Qry <qryseymour@gmail.com>
2025-02-01 15:05:44 -0500

    Some pearls & arrows nerfed

--- kubejs/startup_scripts/itemPropertyEvents.js
+++ kubejs/startup_scripts/itemPropertyEvents.js
@@ -8,4 +8,36 @@ ItemEvents.modification(event => {
     event.modify('endermanoverhaul:corrupted_shield', item => {
         item.maxDamage *= 0.75
     })
+
+    const newPearls = [
+        'endermanoverhaul:corrupted_pearl',
+        'endermanoverhaul:soul_pearl',
+        'endermanoverhaul:ancient_pearl',
+        'endermanoverhaul:bubble_pearl',
+        'endermanoverhaul:summoner_pearl',
+        'endermanoverhaul:icy_pearl',
+        'endermanoverhaul:crimson_pearl',
+        'endermanoverhaul:warped_pearl'
+    ].forEach(pearl => {
+        event.modify(pearl, item => {
+            item.maxStackSize = 1
+        }) 
+    })
+
+    const evilArrows = [
+        'xtraarrows:flint_ender_arrow',
+        'xtraarrows:iron_ender_arrow',
+        'xtraarrows:golden_ender_arrow',
+        'xtraarrows:diamond_ender_arrow',
+        'xtraarrows:netherite_ender_arrow',
+        'xtraarrows:flint_vexing_arrow',
+        'xtraarrows:iron_vexing_arrow',
+        'xtraarrows:golden_vexing_arrow',
+        'xtraarrows:diamond_vexing_arrow',
+        'xtraarrows:netherite_vexing_arrow',
+    ].forEach(arrow => {
+        event.modify(arrow, item => {
+            item.maxStackSize = 48
+        }) 
+    })
 })
